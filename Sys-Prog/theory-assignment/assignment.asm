aee07d23f5f4c2d10e2ba6c2fb6086b60741f866
Subhajit Barh <41638148+SubhajitCode@users.noreply.github.com>
2018-08-01 23:35:11 +0530

    Update assignment.asm

--- Sys-Prog/theory-assignment/assignment.asm
+++ Sys-Prog/theory-assignment/assignment.asm
@@ -3,6 +3,7 @@
 MSG1 DB 'ENTER A LINE OF TEXT',0DH,0AH,'$'
 OUT1 DB 'SMALLEST UPPER CASE LETTER IS: $'
 OUT2 DB 'LARGEST UPPER CASE LATTER IS : $'
+OUT3 DB 'NO UPPER CASE LETTER$'
 .CODE
 MAIN:
         MOV AX,@DATA
@@ -12,18 +13,26 @@ MAIN:
         INT 33
         MOV AH,1
         MOV BL,0
-        MOV CL,'Z'
+        MOV CL,127
 L3:     INT 33
         CMP AL,0DH
         JE L1
+        CMP AL,'a'
+        JGE L3
         CMP AL,BL
         JNGE L2
         MOV BL,AL
 L2:     CMP AL,CL
-        JNLE L4
+        JNLE L3
         MOV CL,AL
-L4:     JMP L3
-L1:     MOV AH,9
+        JMP L3
+L1:     CMP BL,0
+        JNE L5
+        MOV AH,9
+        LEA DX,OUT3
+        INT 21H
+        JMP STOP
+L5:     MOV AH,9
         LEA DX,OUT1
         INT 21H
         MOV AH,2
@@ -43,7 +52,7 @@ L1:     MOV AH,9
         INT 33
         MOV DL,0AH
         INT 33
-        MOV AH,76
+STOP:        MOV AH,76
         INT 33
 END MAIN
 
